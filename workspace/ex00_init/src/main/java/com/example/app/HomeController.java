4a7090f7a74a1facbde948ef9b6b422d1870bf73
donghwan12 <yws078@naver.com>
2024-04-29 16:41:16 +0900

    .

--- workspace/ex00_init/src/main/java/com/example/app/HomeController.java
+++ workspace/ex00_init/src/main/java/com/example/app/HomeController.java
@@ -11,10 +11,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
+@Slf4j
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
@@ -33,6 +36,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
+		log.info("Home Controller..'s..invoke");
 		return "home";
 	}
 	
---
+++ workspace/ex01_lombok_dj_juti/src/test/java/com/example/app/Di/DiTests.java
@@ -0,0 +1,50 @@
+package com.example.app.Di;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.example.app.domain.dto.PersonDto;
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Slf4j
+public class DiTests {
+
+	@Autowired
+	private PersonDto personDtoByXml; //root-context에 있는 bean 경로
+	
+	@Autowired
+	private PersonDto personDtoByXml2;
+	@Autowired
+	private PersonDto person03;
+	@Autowired
+	private PersonDto personDtoBean;
+	
+	@Test
+	public void test() {
+		log.info(personDtoByXml.toString());
+		log.info(personDtoByXml2.toString());
+		log.info( person03.toString());
+		log.info( personDtoBean.toString());
+		
+	}
+	@Autowired
+	private ApplicationContext applicationContext;
+	
+	@Test
+	public void BeanTest_02() {
+		log.info(applicationContext.toString());
+		log.info(personDtoByXml.toString());
+		log.info(personDtoByXml2.toString());
+		log.info(person03.toString());
+		log.info(personDtoBean.toString());
+	}
+
+}
